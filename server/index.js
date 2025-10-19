908c7e4031b1604b9722e0a1d927948e791b94d6
0xWyn <metongchubby@gmail.com>
2025-10-19 19:20:53 +0100

    Progress, day one

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// backend entry file
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello from Mesh backend!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
